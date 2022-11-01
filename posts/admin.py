80fb41815f7baaa20fd3b3b0f6a9f3fc2c01b9b0
JasminUsonova <jasminusmonova629gmail.com>
2022-11-01 18:37:33 +0500

    add tags

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from posts.models import Post
+from posts.models import Post, Tag
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ("created_at", "updated_at")
+    filter_horizontal=("tags",)
--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,14 @@
+from enum import unique
+from turtle import title
 from django.db import models
 from django.utils import timezone
+
+
+class Tag(models.Model):
+    title= models.CharField(max_length=255, unique=True)
+
+    def __str___(self):
+        return f"{self.title:.30}"
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -9,6 +18,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(null=True, blank=True)
+    tags = models.ManyToManyField("posts.Tag")
 
     def __str___(self):
         return f"{self.title:.30}"
