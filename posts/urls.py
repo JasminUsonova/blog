841bac143c218291fba45311ea392fd271a1f8d3
JasminUsonova <jasminusmonova629gmail.com>
2022-11-01 17:37:49 +0500

    add blog

---
+++ posts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
+
+from posts.views import PostDetailView, PostListView, UsersPostListView
+
+urlpatterns = [
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/<pk>/", PostDetailView.as_view(), name="post-detail"),
+    path(
+        "posts/author/<username>/", UsersPostListView.as_view(), name="user-post-list"
+    ),
+    path("", RedirectView.as_view(url=reverse_lazy("post-list"), permanent=False)),
+]
